bd597a75bdda2099c222845fe51c5b90c1789d93
Gordon-Magill <gordon.magill@gmail.com>
2022-09-27 15:55:10 -0700

    Added code comments

--- index.js
+++ index.js
@@ -5,44 +5,43 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // Enumerate questions for the user to answer via Inquirer
 const questions = [
-  // WHEN I enter my project title
-  // THEN this is displayed as the title of the README
+
   {
     type: "input",
     message: `Project title: `,
     name: `projectTitle`,
   },
 
-  // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-  // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
   {
     type: "input",
     message: `Project description: `,
     name: `projectDescription`,
   },
+
   {
     type: "input",
     message: `Installation instructions:`,
     name: `projectInstructions`,
   },
+
   {
     type: "input",
     message: `Usage information: `,
     name: `projectUsage`,
   },
+
   {
     type: "input",
     message: `Contribution guidelines: `,
     name: `projectContributionGuidelines`,
   },
+
   {
     type: "input",
     message: `Test instructions: `,
     name: `projectTestInstructions`,
   },
 
-  // WHEN I choose a license for my application from a list of options
-  // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered unde
   {
     type: "list",
     message: `Select project license: `,
@@ -50,16 +49,12 @@ const questions = [
     choices: LICESNSE_TYPES.map((element) => element.name),
   },
 
-  // WHEN I enter my GitHub username
-  // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
   {
     type: "input",
     message: "Github username: ",
     name: "projectGithub",
   },
 
-  // WHEN I enter my email address
-  // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
   {
     type: "input",
     message: "Email: ",
@@ -69,7 +64,6 @@ const questions = [
 
 // Write the actual README.md file to disk after formatting its contents
 function writeToFile(fileName, answers) {
-  // Setting up the mother of all template literals for the actual body of the file
 
   // Write the file to disk and console log a confirmation
   fs.writeFile(`./output/${fileName}`, generateMarkdown(answers), (err) => {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,194 +7,165 @@ LICESNSE_TYPES = [
   },
   {
     name: "Apache 2.0",
-    // mdText: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
     imgLink: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
     licLink: "https://opensource.org/licenses/Apache-2.0",
   },
   {
     name: "Boost Software 1.0",
-    // mdText: `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`,
     imgLink: "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
     licLink: "https://www.boost.org/LICENSE_1_0.txt",
   },
   {
     name: "BSD 3-Clause",
-    // mdText: `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
     imgLink: "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
     licLink: "https://opensource.org/licenses/BSD-3-Clause",
   },
   {
     name: "BSD 2-Clause",
-    // mdText: `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`,
     imgLink: "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg",
     licLink: "https://opensource.org/licenses/BSD-2-Clause",
   },
   {
     name: "CC0",
-    // mdText: `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`,
     imgLink: "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
     licLink: "http://creativecommons.org/publicdomain/zero/1.0/",
   },
   {
     name: "Attribution 4.0 International",
-    // mdText: `[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)`,
     imgLink: "https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg",
     licLink: "https://creativecommons.org/licenses/by/4.0/",
   },
   {
     name: "Attribution-ShareAlike 4.0 International",
-    // mdText: `[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)`,
     imgLink: "https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg",
     licLink: "https://creativecommons.org/licenses/by-sa/4.0/",
   },
   {
     name: "Attribution-NonCommercial 4.0 International",
-    // mdText: `[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)`,
     imgLink: "https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
     licLink: "https://creativecommons.org/licenses/by-nc/4.0/",
   },
   {
     name: "Attribution-NoDerivates 4.0 International",
-    // mdText: `[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)`,
+    imgLink: "https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg",
+    licLink: "https://creativecommons.org/licenses/by-nd/4.0/",
+
   },
   {
     name: "Attribution-NonCommmercial-ShareAlike 4.0 International",
-    // mdText: `[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)`,
     imgLink: "https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg",
     licLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
   },
   {
     name: "Attribution-NonCommercial-NoDerivatives 4.0 International",
-    // mdText: `[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)`,
     imgLink:
       "https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg",
     licLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
   },
   {
     name: "Eclipse Public 1.0",
-    // mdText: `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`,
     imgLink: "https://img.shields.io/badge/License-EPL_1.0-red.svg",
     licLink: "https://opensource.org/licenses/EPL-1.0",
   },
   {
     name: "GNU GPL v3",
-    // mdText: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
     imgLink: "https://img.shields.io/badge/License-GPLv3-blue.svg",
     licLink: "https://www.gnu.org/licenses/gpl-3.0",
   },
   {
     name: "GNU GPL v2",
-    // mdText: `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`,
     imgLink: "https://img.shields.io/badge/License-GPL_v2-blue.svg",
     licLink: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
   },
   {
     name: "GNU AGPL v3",
-    // mdText: `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`,
     imgLink: "https://img.shields.io/badge/License-AGPL_v3-blue.svg",
     licLink: "https://www.gnu.org/licenses/agpl-3.0",
   },
   {
     name: "GNU LGPL v3",
-    // mdText: `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`,
     imgLink: "https://img.shields.io/badge/License-LGPL_v3-blue.svg",
     licLink: "https://www.gnu.org/licenses/lgpl-3.0",
   },
   {
     name: "GNU FDL v1.3",
-    // mdText: `[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)`,
     imgLink: "https://img.shields.io/badge/License-FDL_v1.3-blue.svg",
     licLink: "https://www.gnu.org/licenses/fdl-1.3",
   },
   {
     name: "Hippocratic 2.1",
-    // mdText: `[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)`,
     imgLink:
       "https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg",
     licLink: "https://firstdonoharm.dev",
   },
   {
     name: "Hippocratic 3.0",
-    // mdText: `[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)`,
     imgLink:
       "https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg",
     licLink: "https://firstdonoharm.dev",
   },
   {
     name: "IBM Public 1.0",
-    // mdText: `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`,
     imgLink: "https://img.shields.io/badge/License-IPL_1.0-blue.svg",
     licLink: "https://opensource.org/licenses/IPL-1.0",
   },
   {
     name: "ISC",
-    // mdText: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
     imgLink: "https://img.shields.io/badge/License-ISC-blue.svg",
     licLink: "https://opensource.org/licenses/ISC",
   },
   {
     name: "MIT",
-    // mdText: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
     imgLink: "https://img.shields.io/badge/License-MIT-yellow.svg",
     licLink: "https://opensource.org/licenses/MIT",
   },
   {
     name: "Mozilla Public 2.0",
-    // mdText: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
     imgLink: "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
     licLink: "https://opensource.org/licenses/MPL-2.0",
   },
   {
     name: "Attribution (BY)",
-    // mdText: `[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)`,
     imgLink: "https://img.shields.io/badge/License-ODC_BY-brightgreen.svg",
     licLink: "https://opendatacommons.org/licenses/by/",
   },
   {
     name: "Open Database (ODbL)",
-    // mdText: `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`,
     imgLink: "https://img.shields.io/badge/License-ODbL-brightgreen.svg",
     licLink: "https://opendatacommons.org/licenses/odbl/",
   },
   {
     name: "Public Domain Dedication and License (PDDL)",
-    // mdText: `[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)`,
     imgLink: "https://img.shields.io/badge/License-PDDL-brightgreen.svg",
     licLink: "https://opendatacommons.org/licenses/pddl/",
   },
   {
     name: "Perl",
-    // mdText: `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
     imgLink: "https://img.shields.io/badge/License-Perl-0298c3.svg",
-    licLink: "https://opensource.org/licenses/Artistic-2.0",
+    licLink: "https://opensource.org/licenses/Artistic-Perl-1.0",
   },
   {
     name: "Artistic 2.0",
-    // mdText: `[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
     imgLink: "https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg",
     licLink: "https://opensource.org/licenses/Artistic-2.0",
   },
   {
     name: "SIL Open Font 1.1",
-    // mdText: `[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)`,
     imgLink: "https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg",
     licLink: "https://opensource.org/licenses/OFL-1.1",
   },
   {
     name: "Unlicense",
-    // mdText: `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`,
     imgLink: "https://img.shields.io/badge/license-Unlicense-blue.svg",
     licLink: "http://unlicense.org/",
   },
   {
     name: "Do What the Fuck You Want to Public License",
-    // mdText: `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`,
     imgLink: "https://img.shields.io/badge/License-WTFPL-brightgreen.svg",
     licLink: "http://www.wtfpl.net/about/",
   },
   {
     name: "zlib/libpng",
-    // mdText: `[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)`,
     imgLink: "https://img.shields.io/badge/License-Zlib-lightgrey.svg",
     licLink: "https://opensource.org/licenses/Zlib",
   },
@@ -204,13 +175,15 @@ LICESNSE_TYPES = [
 // and returns the corresponding MD text for that license's badge.
 // If there is no license, returns an empty string.
 function renderLicenseBadge(license) {
-  // Could be done with switch/case, but this is more compact
   let output = "";
 
+  // Preempt the rest of the function execution if there is no license
   if (license === "None") {
-    return "";
+    return output;
   }
 
+  // Could be done with switch/case, but this is more compact
+  // Generate the MD from the link and image link
   LICESNSE_TYPES.forEach((element) => {
     if (element.name === license) {
       output = `[![License: ${element.name}](${element.imgLink})](${element.licLink})`;
@@ -220,15 +193,18 @@ function renderLicenseBadge(license) {
   return output;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
+// If there is no license, returns an empty string
 function renderLicenseLink(license) {
   let output = "";
 
+  // Special case for no license
   if (license === "None") {
-    return "";
+    return output;
   }
 
+  // Could be done with switch/case, but this is more compact
+  // Generates the MD link
   LICESNSE_TYPES.forEach((element) => {
     if (element.name === license) {
       output = `[${element.name}](${element.licLink})`;
@@ -238,25 +214,30 @@ function renderLicenseLink(license) {
   return output;
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  // No license string
   noLicense = `## License
 
 This code is not licensed in any way.`;
 
+  // String for if there is a license
   yesLicense = `## License
 
 Licensed under ${renderLicenseLink(license)}.`;
 
+  // Return the correct string
   if (license === "None") {
     return noLicense;
   }
   return yesLicense;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate the actual md
 function generateMarkdown(answers) {
+
+  // Generate the mother of all template literals for the bulk of the md
   const readmeContent = `# ${answers.projectTitle}
 ${renderLicenseBadge(answers.projectLicense)}
 
@@ -309,4 +290,5 @@ ${renderLicenseSection(answers.projectLicense)}
   return readmeContent;
 }
 
+// Hide all the functionality in this package so index.js is clean
 module.exports = generateMarkdown;
